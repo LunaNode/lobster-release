2770d87ee87eb5463e9fe85b25ba9c53f00cce9e
Favyen Bastani <fbastani@perennate.com>
2015-06-10 17:18:00 -0400

    Add noVNC and update NOTICE to include novnc/golang dependencies.

--- prepare-release.sh
+++ prepare-release.sh
@@ -6,7 +6,7 @@ go build src/github.com/LunaNode/lobster/cmd/lobster.go
 # copy files
 cp src/github.com/LunaNode/lobster/INSTALL .
 cp src/github.com/LunaNode/lobster/LICENSE .
-cp src/github.com/LunaNode/lobster/NOTICE .
+# (skip notice, since we need to mention other libraries)
 cp src/github.com/LunaNode/lobster/README .
 cp src/github.com/LunaNode/lobster/schema.sql .
 cp src/github.com/LunaNode/lobster/lobster.example* .
@@ -14,3 +14,8 @@ rm -r assets docs tmpl
 cp -r src/github.com/LunaNode/lobster/assets/ assets/
 cp -r src/github.com/LunaNode/lobster/docs/ docs/
 cp -r src/github.com/LunaNode/lobster/tmpl/ tmpl/
+
+# fetch files
+rm -rf novnc
+git clone https://github.com/kanaka/noVNC.git novnc
+rm -rf novnc/.git
