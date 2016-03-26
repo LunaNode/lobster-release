7626badb8506d8c32f85bc64c36d1773348fc42f
Favyen Bastani <fbastani@perennate.com>
2016-03-26 00:04:31 -0400

    Lobster build Sat Mar 26 00:04:28 EDT 2016

--- prepare-release.sh
+++ prepare-release.sh
@@ -3,15 +3,17 @@
 export GOPATH=`pwd`
 go build src/github.com/LunaNode/lobster/cmd/lobster.go
 mv lobster lobster_`uname -m`
+go build src/github.com/LunaNode/lobster/cmd/manage.go
+mv manage manage_`uname -m`
 
 # copy files
 cp src/github.com/LunaNode/lobster/INSTALL .
 cp src/github.com/LunaNode/lobster/LICENSE .
 # (skip notice, since we need to mention other libraries)
 cp src/github.com/LunaNode/lobster/README .
-cp src/github.com/LunaNode/lobster/schema.sql .
 cp src/github.com/LunaNode/lobster/lobster.example* .
-rm -r assets tmpl language wssh
+rm -r db assets tmpl language wssh
+cp -r src/github.com/LunaNode/lobster/db/ db/
 cp -r src/github.com/LunaNode/lobster/assets/ assets/
 cp -r src/github.com/LunaNode/lobster/tmpl/ tmpl/
 cp -r src/github.com/LunaNode/lobster/language/ language/
